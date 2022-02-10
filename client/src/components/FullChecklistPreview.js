777dc1767a89304dc7bd451ffc184d903cf178b2
Kenneth Kim <kkm2059@gmail.com>
2022-02-10 12:33:58 -0500

    Added Following on ViewChecklist

--- client/src/components/FullChecklistPreview.js
+++ client/src/components/FullChecklistPreview.js
@@ -10,6 +10,9 @@ function FullChecklistPreview({setCurrentUser, currentUser}) {
     const [renderTasks, setRenderTasks] = useState()
     const [currentTasks, setCurrentTasks] = useState([])
     const [creator, setCreator] = useState("")
+    const [followsLoaded, setFollowsLoaded] = useState(false)
+    const [followsRendered, setFollowsRendered] = useState([])
+    const [usersFollows, setUsersFollows] = useState(false)
     let {id} = useParams()
     useEffect(() => {
         fetch(`http://localhost:3000/checklists/${id}`)
@@ -21,6 +24,7 @@ function FullChecklistPreview({setCurrentUser, currentUser}) {
                        fetch(`http://localhost:3000/users/${data.user_id}`)
                        .then(resp => resp.json())                       
                        .then(user => setCreator(user))
+                    //    .then(() => setLoaded(true))
         })
     },[])
     function loadPage() {
@@ -31,14 +35,60 @@ function FullChecklistPreview({setCurrentUser, currentUser}) {
         }
     }
 
+    function checkFollows() {
+        if (currentUser == undefined){
+        console.log("no user yet")
+        } else if (followsLoaded==false ){
+            fetch(`http://localhost:3000/users/${currentUser.id}/checklistIFollow`)
+            .then(resp => resp.json())
+            .then(data => {setFollowsRendered(data)
+            })
+
+            fetch(`http://localhost:3000/users/${currentUser.id}/checklist_follows`)
+            .then(resp => resp.json())
+            .then(data => setUsersFollows(data))
+
+            setFollowsLoaded(true)
+        }
+    }
+
+    function handleFollow() {
+        const data = {
+            checklist_id: currentChecklist.id,
+            user_id: currentUser.id
+        }
+        // setLoaded(false)
+        fetch(`http://localhost:3000/checklist_follows`, {
+            method: "POST",
+            headers:{"Content-Type": "application/json"},
+            body: JSON.stringify(data) 
+        })
+        .then(() => window.location.reload())
+    }
+
+    function handleUnfollow() {
+        console.log(currentChecklist.id)
+        console.log(currentUser.id)
+        console.log(usersFollows)
+        const targetedUnfollow = usersFollows.find(f => f.checklist_id == currentChecklist.id && f.user_id == currentUser.id).id
+        console.log(targetedUnfollow)
+
+        fetch(`http://localhost:3000/checklist_follows/${targetedUnfollow}`, {
+            method: "DELETE"
+        })
+        .then(window.location.reload())
+    }
+
     // const loadCreator = creator ? <h3>Created By: {creator.username}</h3>} : undefined
 
     return (
         <div>
+            {checkFollows()}
             {loadPage()}
             {currentChecklist ? <div>
                 <NavBar setCurrentUser={setCurrentUser} currentUser={currentUser}/>
                 <div className="FullChecklistPreview">
+            {loaded ? (currentUser ? (currentUser.id == currentChecklist.user_id ? undefined : ( followsRendered.find(c => c.id == currentChecklist.id) ? <button onClick={() => handleUnfollow()}>x</button> : <button onClick={() => handleFollow()}>+</button>  ) ) : undefined) : undefined}
                 <h1>{currentChecklist.title}</h1>
                 {creator ? <h3>Created By: {creator.username}</h3> : undefined}
                 {renderChecklist}
